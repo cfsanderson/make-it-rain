c38ec936a7f45bdd5b73cb3c5a3e99531e2cd2cf
Caleb Frost Sanderson <calebsanderson@gmail.com>
2016-11-26 20:40:25 -0500

    the drops are falling

--- public/main.js
+++ public/main.js
@@ -1,35 +1,19 @@
 const $ = s => document.querySelector(s)
 const $$ = s => document.querySelectorAll(s)
 
-// create the precipitation on button click
+// CreateDrop function:
+//   Create a new drop
+//   Append it to the drops container
 
 const createDrops = () => {
   console.log('drops')
   const createLi = document.createElement('li')
+  createLi.style.top = '0px'
+  createLi.style.left = `${Math.round(document.body.clientWidth * Math.random())}px`
   const createFigure = document.createElement('figure')
   createLi.appendChild(createFigure)
   createFigure.className = 'newDrops'
   $('ul.drops').appendChild(createLi)
-  const falling = () => {
-    console.log('falling')
-  }
-
-  // $('.newDrops')
-  //
-  // setInterval(() => {
-  //   box.style.top = '-50px' // moves it -50px from the top, off the page
-  //   box.style.left = '100px' // moves it left 100px
-  // }, 12000) // 12000 sets the amount of falling time
-  //
-  // const animate = () => {
-  //   box.style.top = `${parseInt(box.style.top) + 2}px` // controls the rate of fall
-  // }
-  //
-  // setInterval(animate, 16.66)
-  //
-  // setTimeout(() => {
-  //   box.style.backgroundColor = 'green'
-  // }, 5000)
 }
 
 const createFlakes = () => {
@@ -41,72 +25,42 @@ const createFlakes = () => {
   $('ul.flakes').appendChild(li)
 }
 
-// listen for button clicks
+// Animate function:
+//   For every drop in the drops container,
+//     Increase it's 'top' property
+
+// const animate = () => {
+//   console.log('animate')
+//   createLi.style.top = `${parseInt(createLi.style.top) + 1}px` // controls the rate of fall
+// }
+
+// setInterval(() => {
+//   createLi.style.top = '-50px' // moves it -50px from the top, off the page
+//   createLi.style.left = '100px' // moves it left 100px should be randomized
+// }, 5000) // sets the reset time
+//
+// setInterval(animate, 16.66) // sets the interval for when we call animal again
+
+// Main function:
+//   When a button is clicked, call CreateDrop function
+//   At an interval, call Animate function
+
+const animate = () => {
+  const drops = $$('.drops li')
+  for (var i = 0; i < drops.length; i++) {
+    drops[i].style.top = `${parseInt(drops[i].style.top) + 1}px`
+  }
+}
 
 const main = () => {
   $('button.makeItRain').addEventListener('click', createDrops)
   $('button.makeItSnow').addEventListener('click', createFlakes)
+
+  setInterval(animate, 16.6) // sets the reset time
+
+  // setInterval(animate, 16.66) // sets the interval for when we call animal again
 }
 
-const box = $('.drop1')
-
-setInterval(() => {
-  box.style.top = '-50px' // moves it -50px from the top, off the page
-  box.style.left = '100px' // moves it left 100px should be randomized
-}, 5000) // sets the reset time
-
-const animal = () => {
-  box.style.top = `${parseInt(box.style.top) + 2}px` // controls the rate of fall
-}
-
-setInterval(animal, 16.66) // sets the interval for when we call animal again
-
-setTimeout(() => {
-  // box.style.backgroundColor = 'green'
-}, 1000)
+// When document is ready, call Main function
 
 document.addEventListener('DOMContentLoaded', main)
-
-// make a raindrop "pop" into existence at a random (math.random?) place on the x axis at top of the page
-
-// and drop off the visible portion of the page with increasing velocity
-// set timeout on 'click' and 'doubleclick' event listeners to change from raindrop to snowflake?
-// get a random
-
-// const update = () => {
-//   const hue = qs('input[name=hue]').value
-//   const saturation = qs('input[name=saturation]').value
-//   const lightness = qs('input[name=lightness]').value
-//   const alpha = qs('input[name=alpha]').value
-//   const hsla = `hsla(${hue}, ${saturation}%, ${lightness}%, ${alpha})`
-//   qs('h2').textContent = hsla
-//   qs('.picker figure').style.backgroundColor = hsla
-// }
-//
-// const randomize = () => {
-//   qs('input[name=hue]').value = Math.round(Math.random() * 360)
-//   qs('input[name=saturation]').value = Math.round(Math.random() * 100)
-//   qs('input[name=lightness]').value = Math.round(Math.random() * 100)
-//   qs('input[name=alpha]').value = 1
-//   update()
-// }
-//
-// const save = () => {
-//   const rain = $('button[name=makerain]').value
-//   const hsla = `hsla(${hue}, ${saturation}%, ${lightness}%, ${alpha})`
-//   const li = document.createElement('li')
-//   const figure = document.createElement('figure')
-//   figure.style.backgroundColor = hsla
-//   li.appendChild(figure)
-//   qs('ul.library').appendChild(li)
-// }
-//
-// const main = () => {
-//   const inputs = qsa('.picker input')
-//   for (var i = 0; i < inputs.length; i++) {
-//     inputs[i].addEventListener('input', update)
-//   }
-//   qs('.actions .random').addEventListener('click', randomize)
-//   qs('.actions .save').addEventListener('click', save)
-//   update()
-// }
